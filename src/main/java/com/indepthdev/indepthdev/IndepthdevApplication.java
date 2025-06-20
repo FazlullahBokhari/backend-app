7e3397bea7332dec18b89d01c90ad333f7a72c77
FazlullahBokhari <fazlullahb@gmail.com>
2025-06-20 14:00:36 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/indepthdev/indepthdev/IndepthdevApplication.java
@@ -0,0 +1,13 @@
+package com.indepthdev.indepthdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndepthdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndepthdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indepthdev/indepthdev/IndepthdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indepthdev.indepthdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndepthdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
